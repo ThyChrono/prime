84d8c7c44805cdf941dd1deb30d229379faf3c03
TheAddai <agbonicholas1965@gmail.com>
2024-06-29 21:30:33 +0000

    window events

---
+++ prime/src/prime/core/events.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "defines.h"
+
+namespace prime::core {
+
+	using WindowHandle = void*;
+
+	class WindowResizeEvent
+	{
+	private:
+		u32 m_width = 0, m_height = 0;
+
+	public:
+		WindowResizeEvent(u32 width, u32 height)
+			: m_width(width), m_height(height) {}
+
+		u32 getWidth() const { return m_width; }
+		u32 getHeight() const { return m_height; }
+	};
+
+	class WindowCloseEvent
+	{
+	private:
+		WindowHandle m_handle = nullptr;
+
+	public:
+		WindowCloseEvent(WindowHandle handle) : m_handle(handle) {}
+
+		const WindowHandle GetHandle() { return m_handle; }
+	};
+}
